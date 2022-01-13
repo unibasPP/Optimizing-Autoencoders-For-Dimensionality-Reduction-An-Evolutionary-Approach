671ad2b3b2732a9e57a56162ffe5753240623a43
unibasPP <97582321+unibasPP@users.noreply.github.com>
2022-01-14 00:15:37 +0100

    Add files via upload

---
+++ Experiments/ExpCV/SwissRollCV/SR_rand.py
@@ -0,0 +1,80 @@
+
+"""
+Experiment2: 
+Strategy: jDE/rand/1
+Data:     swiss roll    
+"""
+
+# imports
+#----------
+
+import numpy as np
+import numpy.random as rd
+import matplotlib.pyplot as plt
+from frameworkDE.optimizer import DENN
+from frameworkDE.functions import Sigmoid, MSE
+import Experiments.plot as p
+from sklearn.decomposition import PCA 
+import pandas as pd
+import seaborn as sns
+from copy import deepcopy
+import Experiments.saveLoad as s
+
+
+# =============================================================================
+# Swiss Roll
+# =============================================================================
+
+#------------------------------------------------------------------------------
+# load data 
+#------------------------------------------------------------------------------
+df   = s.load('Experiments\Data\swissroll.dat')
+# data X_scaled scaled with min max 
+X_scaled, X_split, y_split = df # X_scaled = train, test, vali / y = train, test, vali
+
+#------------------------------------------------------------------------------
+# import random seeds for experiment (fixed in all experiments)
+#------------------------------------------------------------------------------
+seeds = s.load('Experiments\Data\seeds.dat')
+
+
+# Experiment
+#------------------------------------------------------------------------------ 
+# 1. [jDE/rand/1]
+#------------------------------------------------------------------------------
+
+# Parameter settings 
+#-------------------
+n_nodes        = [3, 8, 2, 8, 3]
+activationList = [Sigmoid(), Sigmoid(), Sigmoid(), Sigmoid()]
+loss           = MSE()
+niter          = 10000
+popSize        = 100
+strat          = 'rand'
+jDE            = True
+
+# save all trained model in list
+models     = []
+
+# experiment run 10 times with 10000 iteration 
+for i in range(10):
+    # scaled values
+    X_tr_mm, X_te_mm, X_va_mm = X_scaled[i]
+    # set seed
+    rd.seed(seeds[i])
+    # create model accoring to Parameters
+    AE = DENN(popSize, n_nodes, activationList, loss, niter, strat, jDE=jDE) 
+    # do process
+    best = AE.evolution(X_tr_mm, X_va_mm, X_te_mm)
+    models.append(deepcopy(AE))
+
+
+# =============================================================================
+# s.save(models, 'SR_rand.dat') # Experiments/result_SR
+# =============================================================================
+xxx = s.load('SR_rand.dat')
+
+
+
+
+
